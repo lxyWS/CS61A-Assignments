8db538a4fa50d84376786c5d21c173d0b1ed8e78
lxyWS <348677673@qq.com>
2025-08-10 20:08:57 +0800

    hw07 finished

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,26 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  ; 'YOUR-CODE-HERE
+  (if (= exp 0)
+      1
+      (if (even? exp)
+          (square (pow base (/ exp 2)))
+          (* base (square (pow base (/ (- exp 1) 2)))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y
+            (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+    ;'YOUR-CODE-HERE
+    (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+    ;'YOUR-CODE-HERE
+    (car (cddr s)))
