68f66e0ff5167042777ed9161ad64553dfe9a2c1
lxyWS <348677673@qq.com>
2025-08-10 11:16:55 +0800

    lab08 finished

--- Lab/lab08/lab08.py
+++ Lab/lab08/lab08.py
@@ -12,6 +12,9 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
+    for child in t.branches:
+        cumulative_mul(child)
+        t.label *= child.label
 
 
 def prune_small(t, n):
@@ -31,11 +34,11 @@ def prune_small(t, n):
     >>> t3
     Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
     """
-    while ____:
-        largest = max(____, key=____)
+    while len(t.branches) > n:
+        largest = max(t.branches, key=lambda x: x.label)
         t.branches.remove(largest)
     for b in t.branches:
-        ____
+        prune_small(b, n)
 
 
 def delete(t, x):
@@ -58,13 +61,13 @@ def delete(t, x):
     Tree(1, [Tree(4), Tree(5), Tree(3, [Tree(6)]), Tree(6), Tree(7), Tree(8), Tree(4)])
     """
     new_branches = []
-    for _________ in ________________:
-        _______________________
+    for b in t.branches:
+        delete(b, x)
         if b.label == x:
-            __________________________________
+            new_branches += b.branches
         else:
-            __________________________________
-    t.branches = ___________________
+            new_branches.append(b)
+    t.branches = new_branches
 
 
 def max_path_sum(t):
@@ -75,6 +78,13 @@ def max_path_sum(t):
     11
     """
     "*** YOUR CODE HERE ***"
+    if t.branches != []:
+        sums= []
+        for b in t.branches:
+            sums.append(max_path_sum(b))
+        return max(sums) + t.label
+    else:
+        return t.label
 
 
 class Tree:
