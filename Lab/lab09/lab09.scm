77e8e33f2a388af66724ad23f1e262520313d428
lxyWS <348677673@qq.com>
2025-08-10 17:38:42 +0800

    lab09 finished

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,32 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+; (define (over-or-under num1 num2)
+;  (if (< num1 num2)
+;      -1
+;      (if (= num1 num2)
+;          0
+;          1)))
+(define (over-or-under num1 num2)
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    (else          1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  ; 'YOUR-CODE-HERE
+  (define (adds x) (+ num x))
+  adds)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  ; 'YOUR-CODE-HERE
+  (define (cps_func x) (f (g x)))
+  cps_func)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  ; 'YOUR-CODE-HERE
+  (define (func x)
+    (if (> n 1)
+        (f ((repeat f (- n 1)) x))
+        (f x)))
+  func)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +38,10 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  ; 'YOUR-CODE-HERE
+  (define c (max a b))
+  (define d (min a b))
+  (if (= (modulo c d) 0)
+      d
+      (gcd d (modulo c d))))
