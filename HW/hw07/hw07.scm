43d82dd1ac4a2e40d70a4524a5f19455068e6c8a
lxyWS <348677673@qq.com>
2025-08-11 11:42:57 +0800

    hw08 finished

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -11,16 +11,40 @@
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let ((y
-            (repeatedly-cube (- n 1) x)))
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 
-    ;'YOUR-CODE-HERE
-    (car (cdr s)))
+(define (cadr s)
+  ; 'YOUR-CODE-HERE
+  (car (cdr s)))
 
-(define (caddr s) 
-    ;'YOUR-CODE-HERE
-    (car (cddr s)))
+(define (caddr s)
+  ; 'YOUR-CODE-HERE
+  (car (cddr s)))
+
+(define (even-subsets s)
+  (if (null? s)
+      nil
+      (append (even-subsets (cdr s))
+              (map (lambda (t) (cons (car s) t))
+                   (if (even? (car s))
+                       (even-subsets (cdr s))
+                       (odd-subsets (cdr s))))
+              (if (even? (car s))
+                  ;(list (list (car s)))
+                  (cons (cons (car s) nil) nil)
+                  nil))))
+
+(define (odd-subsets s)
+  (if (null? s)
+      nil
+      (append (odd-subsets (cdr s))
+              (map (lambda (t) (cons (car s) t))
+                   (if (odd? (car s))
+                       (even-subsets (cdr s))
+                       (odd-subsets (cdr s))))
+              (if (odd? (car s))
+                  (list (list (car s)))
+                  nil))))
--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,34 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  ; 'YOUR-CODE-HERE
+  (if (or (null? s) (null? (cdr s)))
+      #t
+      (and (<= (car s) (car (cdr s)))
+           (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  ; 'YOUR-CODE-HERE
+  (if (null? s)
+      nil
+      (if (pred (car s))
+          (cons (car s) (my-filter pred (cdr s)))
+          (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  ; 'YOUR-CODE-HERE
+  (if (null? lst1)
+      (if (null? lst2)
+          nil
+          lst2)
+      (if (null? lst2)
+          lst1
+          (cons (car lst1)
+                (cons (car lst2)
+                      (interleave (cdr lst1) (cdr lst2)))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+  ; 'YOUR-CODE-HERE
+  (if (null? s)
+      nil
+      (cons (car s)
+            (no-repeats
+             (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
